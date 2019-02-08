f165bbc7e74ec075037d75ba96a896cf0e9ba52c
BENK <Ben.King@tabs3.com>
2019-02-08 10:53:08 -0600

    Add personal build process
    To more easily work with the other repositories I use flot in, building
    a non-minified version is preferable for the debug-only work I'll be
    utilizing this build process for.

--- gulpfile.js
+++ gulpfile.js
@@ -41,3 +41,35 @@ gulp.task('build_engineering_flot', function() {
         .pipe(maps.write('./'))
         .pipe(gulp.dest('dist/es5'));
 });
+
+gulp.task('build_debug', function() {
+    return gulp.src([
+        './source/jquery.canvaswrapper.js',
+        './source/jquery.colorhelpers.js',
+        './source/jquery.flot.js',
+        './source/jquery.flot.saturated.js',
+        './source/jquery.flot.browser.js',
+        './source/jquery.flot.drawSeries.js',
+        './source/jquery.flot.errorbars.js',
+        './source/jquery.flot.uiConstants.js',
+        './source/jquery.flot.logaxis.js',
+        './source/jquery.flot.symbol.js',
+        './source/jquery.flot.flatdata.js',
+        './source/jquery.flot.navigate.js',
+        './source/jquery.flot.fillbetween.js',
+        './source/jquery.flot.stack.js',
+        './source/jquery.flot.touchNavigate.js',
+        './source/jquery.flot.hover.js',
+        './source/jquery.flot.touch.js',
+        './source/jquery.flot.time.js',
+        './source/jquery.flot.axislabels.js',
+        './source/jquery.flot.selection.js',
+        './source/jquery.flot.composeImages.js',
+        './source/jquery.flot.legend.js'
+        ])
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(concat('jquery.flot.js'))
+        .pipe(gulp.dest('dist/debug_es5'));
+});
